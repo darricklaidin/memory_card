7e32111215ee24f10fe2f4efd5ac7f752be18ae2
darricklaidin <darricklaidin@gmail.com>
2022-12-13 18:46:01 +0700

    Add static UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Card from './components/Card';
+import Modal from './components/Modal'
+
+const data = [
+  { name: "Phantom" },
+  { name: "Vandal" },
+  { name: "Ares" },
+  { name: "Bulldog" },
+  { name: "Specter" },
+  { name: "Marshal" },
+  { name: "Stinger" },
+  { name: "Operator" },
+  { name: "Ghost" },
+  { name: "Frenzy" },
+  { name: "Odin" },
+  { name: "Guardian" },
+  { name: "Bucky" },
+  { name: "Judge" },
+  { name: "Classic" },
+  { name: "Shorty" },
+  { name: "Sheriff" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [isDisplayed, setIsDisplayed] = useState(false);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  
+  // playable cards
+  // seen cards
+  // unseen cards
+  
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Memory Card</h1>
+      <p className="scoreRow">Score: {score} | High score: {highScore}</p>
+      <div className="cardContainer">
+        {/* Map generated */}
+        <Card name={"Phantom"} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <Modal isDisplayed={isDisplayed} />
+      
     </div>
   )
 }
