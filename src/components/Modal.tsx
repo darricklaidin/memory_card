55ab483a6001f25369b3071cf7a03ed131f5483c
darricklaidin <darricklaidin@gmail.com>
2022-12-14 19:15:21 +0700

    Implement dynamic modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,22 +1,17 @@
 import React from 'react'
 
 export default function Modal(props: any) {
-  // game result
-  // score
-  // high score
-  // should display
-  
-  const { isDisplayed } = props;
+  const { isDisplayed, gameResult, score, highScore, handleRestartClick } = props;
   
   if (!isDisplayed) return null;
   
   return (
     <div className="gameOverModalBackground">
       <div className="gameOverModal">
-        <p>You {"win"}</p>
-        <p>Score: {5}</p>
-        <p>High score: {10}</p>
-        <button>Play again</button>
+        <p>You {gameResult ? "win" : "lose"}</p>
+        <p>Score: {score}</p>
+        <p>High score: {highScore}</p>
+        <button onClick={handleRestartClick}>Play again</button>
       </div>
     </div>
   )
