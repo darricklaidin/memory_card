ee5e841de55f932cf9a4e1cd3f874174b8fb81d7
darricklaidin <darricklaidin@gmail.com>
2022-12-15 10:51:37 +0700

    Change game over message

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,7 @@ export default function Modal(props: any) {
   return (
     <div className="gameOverModalBackground">
       <div className="gameOverModal">
-        <p>You {gameResult ? "win" : "lose"}</p>
+        <p>You {gameResult ? "win" : "lose"}!</p>
         <p>Score: {score}</p>
         <p>High score: {highScore}</p>
         <button onClick={handleRestartClick}>Play again</button>
